345aed920ec632e9c3991de32dddc4bb6192e16e
lazyduo <dadanuna@gmail.com>
2022-02-07 23:40:54 +0900

    update

--- linux/crontab/jupyter-start.sh
+++ linux/crontab/jupyter-start.sh
@@ -4,6 +4,7 @@ conda info >> /home/smartmind/jupyter-start.log # log append if need
 conda activate rapids-21.10
 
 conda info >> /home/smartmind/jupyter-start.log # log append if need
-/usr/bin/screen -dmS jupyter bash -c 'jupyter lab --allow-root; exec sh'
+/usr/bin/screen -dmS jupyter bash -c 'jupyter lab --allow-root '
+# add ;exec sh command if you don't want to terminate screen at debugging.
 # open screen name jupyter and with detached mode, then bash command follow
 # remember! not 'screen', '/usr/bin/screen'
