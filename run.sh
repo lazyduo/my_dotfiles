c5285def8a7320b5d07ba7777c5c2d9191c27354
Dada Ahn <71164591+lazyduo@users.noreply.github.com>
2022-04-14 16:57:24 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+help()
+{
+	echo "ThanoSQL Engine launch script."
+	echo
+	echo "optional parameters - [p]"
+	echo
+	echo "-p	Port Config"
+	echo
+}
+
+if [ $# -eq 0 ];
+then
+	uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+	exit 0
+else
+	while getopts ":p:h" opt; do
+		case $opt in
+			p)
+				echo "Launch server at port $OPTARG"
+				uvicorn app.main:app --reload --host 0.0.0.0 --port $OPTARG
+				exit;;
+			h)
+				echo
+				help
+				exit;;
+			\?) # Invalid option
+				echo "Error: Invalid option."
+				echo
+				help
+				exit;;
+			:)  # Missing Argument
+				echo "Error: Option -$OPTARG requires an argument."
+				exit;;
+
+		esac
+	done
+	help
+fi
